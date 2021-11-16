9204e3a0e37cc711f09f4a29046b4115fe86bc69
lindseygreenhill <lgreenhill@college.harvard.edu>
2021-11-15 19:14:15 -0500

    data filtered to after 2020 election

--- data_collection/tweet_data_cleaning.R
+++ data_collection/tweet_data_cleaning.R
@@ -2,22 +2,23 @@ library(readxl)
 library(janitor)
 library(skimr)
 library(rtweet)
+library(lubridate)
 library(caTools)
 library(tidyverse)
 set.seed(1121)
 
 
-load("tweets_1.RData")
-load("tweets_2.RData")
-load("tweets_3.RData")
-load("tweets_4.RData")
-load("tweets_5.RData")
-load("tweets_6.RData")
-load("tweets_7.RData")
-load("tweets_8.RData")
-load("tweets_9.RData")
-load("tweets_10.RData")
-load("tweets_11.RData")
+load("data_collection/tweets_1.RData")
+load("data_collection/tweets_2.RData")
+load("data_collection/tweets_3.RData")
+load("data_collection/tweets_4.RData")
+load("data_collection/tweets_5.RData")
+load("data_collection/tweets_6.RData")
+load("data_collection/tweets_7.RData")
+load("data_collection/tweets_8.RData")
+load("data_collection/tweets_9.RData")
+load("data_collection/tweets_10.RData")
+load("data_collection/tweets_11.RData")
 
 
 tweets_full <- Tweets_1 %>%
@@ -39,20 +40,23 @@ tweets_full <- Tweets_1 %>%
 # I still don't have complete data frame but I am going to clean it anyways
 
 tweets_tidy <- tweets_full %>%
-  select(created_at, screen_name, text, is_retweet, hashtags)
+  select(created_at, screen_name, text, is_retweet, hashtags) %>%
+  filter(is_retweet == FALSE) %>%
+  mutate(date = ymd(str_sub(created_at, 1, 10))) %>%
+  filter(date >= as.Date("2020-11-03"))
 
-# save(tweets_tidy, file = "tweets_complete.RData")
+save(tweets_tidy, file = "data_collection/tweets_complete.RData")
 
 # creating training and testing sets
 
-smp_size <- floor(.7*nrow(tweets_tidy))
+smp_size <- floor(.75*nrow(tweets_tidy))
 train_ind <- sample(seq_len(nrow(tweets_tidy)), size = smp_size)
 
 training_data <- tweets_tidy[train_ind, ]
 testing_data <- tweets_tidy[-train_ind, ]
 
-save(training_data, file = "tweets_training_data.RData")
-save(testing_data, file = "tweets_testing_data.RData")
+save(training_data, file = "data_collection/tweets_training_data.RData")
+save(testing_data, file = "data_collection/tweets_testing_data.RData")
 
 
 # Do we want to keep retweets? I am going to clean for emojies. Not sure if I
